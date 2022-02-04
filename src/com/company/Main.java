dbc5b8b52d0cbf6079cb1e7236506a90e1056d26
Denis-D-S <denisduartedasilva@hotmail.com>
2022-02-04 10:07:11 -0300

    atualizando branch secundario

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEstE1 pro Git - PROBLEMA DE MERGE!");
+        System.out.println("Código branch secundario (linha 6)");
     }
 }
--- src/com/company/Teste1.java
+++ src/com/company/Teste1.java
@@ -1,5 +1,5 @@
 package com.company;
 
 public class Teste1 {
-    //OLá, este é o teste1 Só que com possível problema de Merge
+    //Código branch secundário (linha 4)
 }
