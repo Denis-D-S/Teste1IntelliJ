aa1e9661b5d873b949ccf4cff59ef144bfe03fe2
Denis-D-S <denisduartedasilva@hotmail.com>
2022-02-04 10:05:50 -0300

    atualizando pasta master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Este é o código da terceira pasta");
+        System.out.println("Este é o código da branch Master (linha 6)");
     }
 }
--- src/com/company/Teste1.java
+++ src/com/company/Teste1.java
@@ -1,5 +1,5 @@
 package com.company;
 
 public class Teste1 {
-    //Este é o código da terceira pasta
+    //Este é o código da branch Main (linha 4)
 }
