55a2e0a1679db5a1a5322473f8d0e2e0bd377c7d
Denis-D-S <denisduartedasilva@hotmail.com>
2022-02-04 10:09:26 -0300

    atualizando terceira branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEstE1 pro Git - PROBLEMA DE MERGE!");
+        System.out.println("Código branch terceira (linha 6)");
     }
 }
